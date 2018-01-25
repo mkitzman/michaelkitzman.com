53be23209495fcd820eff51f1fb226f729f320e0
Michael Kitzman <mkitzman@gmail.com>
2018-01-24 19:55:54 -0500

    Updates to home page and work, start anchor animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Home = () => (
     <React.Fragment>
         <h2 className={'App-page-title'}>
             Hi, I'm Mike Kitzman
         </h2>
-        <p  className={'Home-hero-description'}>
+        <p className={'Home-hero-description'}>
             I’m a Principal Front-end/UI Developer at Oracle.
         </p>
-        <p  className={'Home-hero-description'}>
+        <p className={'Home-hero-description'}>
             I'm passionate about building websites, CSS Architecture at
             scale, UX, Accessibility and inclusive design, UI Interactions, 
             semantic markup, and performance.
+        </p>
+        <p className={'Home-hero-description'}>
+            I am a Front-End Engineer with over 15 years of experience building web sites that have been 
+            viewed by <strong>billions of people</strong>. <Link className={'App-fancy-anchor'} to="/about">Learn more about me</Link>.
         </p>
     </React.Fragment>
 );
--- src/components/Work.js
+++ src/components/Work.js
@@ -3,6 +3,13 @@ import React from 'react';
 const Work = () => (
     <React.Fragment>
         <h2 className={'App-page-title'}>Work</h2>
+        <h3>Current Tech Stack</h3>
+        <ul className={'Work-tech-stack-list'}>
+            <li className={'Home-tech-stack-list-item'}>Typescript</li>
+            <li className={'Home-tech-stack-list-item'}>React</li>
+            <li className={'Home-tech-stack-list-item'}>PostCSS</li>
+            <li className={'Home-tech-stack-list-item'}>Webpack</li>
+        </ul>
     </React.Fragment>
 );
 
