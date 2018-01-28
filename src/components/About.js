babe79f3e25e412e8114692b67bb1c567e7b9f62
Michael Kitzman <mkitzman@gmail.com>
2018-01-27 19:09:22 -0500

    First part of about

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,15 @@ import React from 'react';
 const About = () => (
     <React.Fragment>
         <h2>About</h2>
+        <p>
+            I'm a Front-End Engineer with over 20 years of experience building web sites that have 
+            been viewed by billions of people. I primarily work with HTML, CSS and JavaScript.
+        </p>
+        <p>
+            On a personal side I love sports ( Broncos, Lakers, Chelsea, Nationals, Capitals) 
+            trying craft beer (Aslin, The Veil Brewing, Tired Hands, Other Half and Trillium, playing guitar, 
+            music and design.
+        </p>
     </React.Fragment>
 );
 
