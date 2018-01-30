82671ae1ef7b2c83755346e2a65d1e2422c64b51
Michael Kitzman <mkitzman@gmail.com>
2018-01-29 20:18:19 -0500

    Framework for contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,6 +3,28 @@ import React from 'react';
 const Contact = () => (
     <React.Fragment>
         <h2>Contact</h2>
+        <fieldset>
+            <p>say hi!</p>
+
+            <ul>
+                <li>
+                    <label for={'name'}>Name:</label>
+                    <input id={'name'} type={'text'} name={'name'} />
+                </li>
+                <li>
+                    <label for={'name'}>Email:</label>
+                    <input id={'name'} type={'text'} name={'name'} />
+                </li>
+                <li>
+                    <label id={'message-label'}for={'message'}>Message:</label>
+                    <textarea
+                        id="message"
+                        aria-invalid="true"
+                        aria-describedby="message-label"
+                    ></textarea>
+                </li>
+            </ul>
+        </fieldset>
     </React.Fragment>
 );
 
