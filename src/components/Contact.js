8bcfaf5fa8abd23b24a9ea965826ecbb271c02cf
Michael Kitzman <mkitzman@gmail.com>
2018-01-30 20:02:55 -0500

    Styled contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,25 +3,39 @@ import React from 'react';
 const Contact = () => (
     <React.Fragment>
         <h2>Contact</h2>
-        <fieldset>
-            <p>say hi!</p>
-
+        <fieldset className={'Contact-field-set'}>
+            <p>Fill out the form below and I'll get back to you as soon as I can</p>
             <ul>
-                <li>
-                    <label for={'name'}>Name:</label>
-                    <input id={'name'} type={'text'} name={'name'} />
+                <li className={'Contact-list-item'}>
+                    <label for={'name'} className={'Contact-label'}>
+                        Name:
+                    </label>
+                    <input className={'Contact-text-input'} id={'name'} type={'text'} name={'name'} />
                 </li>
-                <li>
-                    <label for={'name'}>Email:</label>
-                    <input id={'name'} type={'text'} name={'name'} />
+                <li className={'Contact-list-item'}>
+                    <label for={'email'} className={'Contact-label'}>
+                        Email:
+                    </label>
+                    <input  className={'Contact-text-input'} id={'email'} type={'text'} name={'email'} />
                 </li>
-                <li>
-                    <label id={'message-label'}for={'message'}>Message:</label>
+                <li className={'Contact-list-item'}>
+                    <label id={'message-label'} for={'message'} className={'Contact-label'}>
+                        Message:
+                    </label>
                     <textarea
                         id="message"
                         aria-invalid="true"
                         aria-describedby="message-label"
+                        className={'Contact-text-area'}
                     ></textarea>
+                </li>
+                <li className={'Contact-list-item'}>
+                    <button
+                        className={'Contact-submit'} value={'Submit'}
+                        onClick={this.handleClick}
+                    >
+                        Submit
+                    </button>
                 </li>
             </ul>
         </fieldset>
