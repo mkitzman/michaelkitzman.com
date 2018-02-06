b866d33fc20b10d8bbd9f23e75e943fc616f4eac
Michael Kitzman <mkitzman@gmail.com>
2018-02-06 16:07:41 -0500

    Add profile photo

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import profilephoto from '../images/mikekitzman.jpg';
 
 const About = () => (
     <React.Fragment>
         <h2>About</h2>
-        <p>
-            I&apos;m a Front-End Engineer with over 20 years of experience building web sites that have 
-            been viewed by billions of people. I primarily work with HTML, CSS and JavaScript.
-        </p>
-        <p>
-            On a personal side I love sports ( Broncos, Lakers, Chelsea, Nationals, Capitals) 
-            trying craft beer (Aslin, The Veil Brewing, Tired Hands, Other Half and Trillium, playing guitar, 
-            music and design.
-        </p>
+        <div className={'About-content'}>
+            <img className={'About-profile-photo'} src={profilephoto} alt={'Michael Kitzman'} />
+            <p>
+                I&apos;m a Front-End Engineer with over 20 years of experience building web sites that have 
+                been viewed by billions of people. I primarily work with HTML, CSS and JavaScript.
+            </p>
+            <p>
+                On a personal side I love sports ( Broncos, Lakers, Chelsea, Nationals, Capitals) 
+                trying craft beer (Aslin, The Veil Brewing, Tired Hands, Other Half and Trillium, playing guitar, 
+                music and design.
+            </p>
+        </div>
     </React.Fragment>
 );
 
