75c05456707acba2534ae3aaa056d183c572c016
Michael Kitzman <mkitzman@gmail.com>
2018-02-19 16:46:49 -0500

    Reduce number of web font versions supported

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
-    'postcss-font-magician': {},
+    'postcss-font-magician': {
+      formats: 'woff',
+      variants: {
+        'Lato': {
+            '300': [],
+            '400': [],
+            '900': []
+        },
+        'Karla': {
+          '300': [],
+          '400': [],
+          '900': []
+        },          
+      }
+    },
     'postcss-cssnext': {
       browsers: ['last 2 versions', '> 5%'],
     },
