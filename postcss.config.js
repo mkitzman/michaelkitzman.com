912b227a6f316b10d212963a834da2a2ffe91fcd
Michael Kitzman <mkitzman@gmail.com>
2018-03-21 15:08:40 -0400

    Try manually loading fonts

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,6 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
-    'postcss-font-magician': {
-      formats: 'woff',
-      variants: {
-        'Lato': {
-            '300': [],
-            '400': [],
-            '900': []
-        },
-        'Karla': {
-          '300': [],
-          '400': [],
-          '900': []
-        },          
-      }
-    },
     'postcss-cssnext': {
       browsers: ['last 2 versions', '> 5%'],
     },
