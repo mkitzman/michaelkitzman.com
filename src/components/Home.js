dc0997dd191312a069d4ba1faf1702bb252fd76e
mkitzman <mkitzman@gmail.com>
2024-10-15 11:41:53 -0400

    quick updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => (
-    <div className={'App-page-content-fadein'}>
-        <h2>
-            Hi, I'm Mike Kitzman
-        </h2>
-        <p>
-            Sr. Frontend Engineer at Medallia. Previously Oracle, AddThis, Yahoo! Sports
-        </p>
-        <p>
-            I primarily work in React, Typescript, CSS and HTML. I'm passionate about web-based UX paradigms, progressive enhancement, responsive design, internationalization, localization, design systems, and accessibility.
-        </p>
-        <p>
-            I have extensive experience building web sites viewed by <strong>billions of people</strong>.
-        </p>
+  <div className={"App-page-content-fadein"}>
+    <h2>Hi, I'm Mike Kitzman</h2>
+    <p>
+      Sr. Frontend Engineer. Previously Teachable, Medallia, Oracle, AddThis,
+      Yahoo! Sports
+    </p>
+    <p>
+      I primarily work in React, Typescript, CSS and HTML. I'm passionate about
+      web-based UX paradigms, progressive enhancement, responsive design,
+      internationalization, localization, design systems, and accessibility.
+    </p>
+    <p>
+      I have extensive experience building web sites viewed by{" "}
+      <strong>billions of people</strong>.
+    </p>
 
-        <p>
-            <Link className={'App-fancy-anchor'} to="/about">Learn more about me</Link>
-        </p>
-    </div>
+    <p>
+      <Link className={"App-fancy-anchor"} to="/about">
+        Learn more about me
+      </Link>
+    </p>
+  </div>
 );
 
 export default Home;
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,115 +1,209 @@
-import React from 'react';
+import React from "react";
 
 const Work = () => (
-    <div className={'App-page-content-fadein'}>
-        <h2>Work</h2>
-        <h3>Current Tech Stack</h3>
-        <ul className={'Work-tech-stack-list'}>
-            <li className={'Work-tech-stack-list-item'}>React</li>
-            <li className={'Work-tech-stack-list-item'}>Styled Components</li>
-            <li className={'Work-tech-stack-list-item'}>Gatsby</li>
-            <li className={'Work-tech-stack-list-item'}>Yarn</li>    
+  <div className={"App-page-content-fadein"}>
+    <h2>Work</h2>
+    <h3>Most Recent Tech Stack</h3>
+    <ul className={"Work-tech-stack-list"}>
+      <li className={"Work-tech-stack-list-item"}>React</li>
+      <li className={"Work-tech-stack-list-item"}>React Testing Library</li>
+      <li className={"Work-tech-stack-list-item"}>Sass</li>
+      <li className={"Work-tech-stack-list-item"}>Storybook</li>
+      <li className={"Work-tech-stack-list-item"}>TypeScript</li>
+      <li className={"Work-tech-stack-list-item"}>Yarn</li>
+    </ul>
+
+    <h3>Full skill set</h3>
+    <ul className={"Work-skills-list"}>
+      <li className={"Work-skills-list-item"}>
+        <h4 className={"Work-skills-list-col-header"}>JavaScript Ecosystem</h4>
+        <ul>
+          <li className={"Work-skills-list-col-list-item"}>React</li>
+          <li className={"Work-skills-list-col-list-item"}>TypeScript</li>
+          <li className={"Work-skills-list-col-list-item"}>
+            Styled Components
+          </li>
+          <li className={"Work-skills-list-col-list-item"}>
+            React Testing Library
+          </li>
+          <li className={"Work-skills-list-col-list-item"}>Yarn</li>
+          <li className={"Work-skills-list-col-list-item"}>Git</li>
+          <li className={"Work-skills-list-col-list-item"}>Node.js, Express</li>
+          <li className={"Work-skills-list-col-list-item"}>Webpack</li>
         </ul>
-
-        <h3>Full skill set</h3>
-        <ul className={'Work-skills-list'}>
-            <li className={'Work-skills-list-item'}>
-                <h4 className={'Work-skills-list-col-header'}>
-                    JavaScript Ecosystem
-                </h4>
-                <ul>
-                    <li className={'Work-skills-list-col-list-item'}>React</li>
-                    <li className={'Work-skills-list-col-list-item'}>TypeScript</li>
-                    <li className={'Work-skills-list-col-list-item'}>Styled Components</li>      
-                    <li className={'Work-skills-list-col-list-item'}>React Testing Library</li>                   
-                    <li className={'Work-skills-list-col-list-item'}>Yarn</li>
-                    <li className={'Work-skills-list-col-list-item'}>Git</li>
-                    <li className={'Work-skills-list-col-list-item'}>Node.js, Express</li>
-                    <li className={'Work-skills-list-col-list-item'}>Webpack</li>
-                </ul>
-            </li>
-            <li className={'Work-skills-list-item'}>
-                <h4 className={'Work-skills-list-col-header'}>
-                    User Interface
-                </h4>
-                <ul>
-                    <li className={'Work-skills-list-col-list-item'}>HTML5</li>
-                    <li className={'Work-skills-list-col-list-item'}>CSS3 (PostCSS, SCSS, LESS)</li>
-                    <li className={'Work-skills-list-col-list-item'}>Responsive & Adaptive Web Design</li>
-                    <li className={'Work-skills-list-col-list-item'}>Performance</li>
-                    <li className={'Work-skills-list-col-list-item'}>Semantic Markup</li>
-                    <li className={'Work-skills-list-col-list-item'}>Coding Standards</li>
-                    <li className={'Work-skills-list-col-list-item'}>Cross Browser Compatibility</li>
-                    <li className={'Work-skills-list-col-list-item'}>UX principles and accessibility</li>
-                    <li className={'Work-skills-list-col-list-item'}>Photoshop, Adobe Illustrator</li>
-                </ul>
-            </li>
-            <li className={'Work-skills-list-item'}>
-                <h4 className={'Work-skills-list-col-header'}>
-                    Additional
-                </h4>
-                <ul>
-                    <li className={'Work-skills-list-col-list-item'}>Restful APIs</li>
-                    <li className={'Work-skills-list-col-list-item'}>Gatsby</li>
-                    <li className={'Work-skills-list-col-list-item'}>PHP</li>
-                    <li className={'Work-skills-list-col-list-item'}>CLI, Linux</li>
-                    <li className={'Work-skills-list-col-list-item'}>Agile, Kanban, Scrum</li>
-                </ul>
-            </li>
+      </li>
+      <li className={"Work-skills-list-item"}>
+        <h4 className={"Work-skills-list-col-header"}>User Interface</h4>
+        <ul>
+          <li className={"Work-skills-list-col-list-item"}>HTML5</li>
+          <li className={"Work-skills-list-col-list-item"}>
+            CSS3 (PostCSS, SCSS, LESS)
+          </li>
+          <li className={"Work-skills-list-col-list-item"}>
+            Responsive & Adaptive Web Design
+          </li>
+          <li className={"Work-skills-list-col-list-item"}>Performance</li>
+          <li className={"Work-skills-list-col-list-item"}>Semantic Markup</li>
+          <li className={"Work-skills-list-col-list-item"}>Coding Standards</li>
+          <li className={"Work-skills-list-col-list-item"}>
+            Cross Browser Compatibility
+          </li>
+          <li className={"Work-skills-list-col-list-item"}>
+            UX principles and accessibility
+          </li>
+          <li className={"Work-skills-list-col-list-item"}>
+            Photoshop, Adobe Illustrator
+          </li>
         </ul>
+      </li>
+      <li className={"Work-skills-list-item"}>
+        <h4 className={"Work-skills-list-col-header"}>Additional</h4>
+        <ul>
+          <li className={"Work-skills-list-col-list-item"}>Restful APIs</li>
+          <li className={"Work-skills-list-col-list-item"}>Gatsby</li>
+          <li className={"Work-skills-list-col-list-item"}>PHP</li>
+          <li className={"Work-skills-list-col-list-item"}>CLI, Linux</li>
+          <li className={"Work-skills-list-col-list-item"}>
+            Agile, Kanban, Scrum
+          </li>
+        </ul>
+      </li>
+    </ul>
 
-        <section className={'Work-history'}>
-             <h3 className={'Work-history-title'}>
-                Medallia - Sr. Design Engineer
-            </h3>
-            <h4 className={'Work-history-sub-title'}>
-                April 2018 - Present
-            </h4>
-            <p>Working on the Alchemy design system used across Medallia and its acquired companies. The design system is built in React and Typescript using Styled-components, tested with React Testing Library and documented on a Gatsby website.</p>
-            <p>Part of a small team that created the component library from scratch. Because of our work Medallia has been able to unite design across their websites by utilizing our pre-made UI React components and elements.</p>
-            <p>Creating a new component allowed me to grow several skill sets beyond my core skills including API design, accessibility with a minimum WCAG 2.0 AA compliance and comprehensive testing using React Testing Library.</p>
-            <p>Extensively documented all components I worked on including examples.</p>
-            <p>Working closely with design on implementing their mocks to insure that they stay true to the design but also take into account possible browser, accessibility and ux issues that may arise.</p>
-            <p>Helped teams onboard to Alchemy by answering questions, code reviews, debugging issues and adding requested features.</p>
+    <section className={"Work-history"}>
+      <h3 className={"Work-history-title"}>
+        Teachable - Sr. Frontend Engineer, Design Systems and Engineering
+        Enablement
+      </h3>
+      <h4 className={"Work-history-sub-title"}>May 2022 - October 2024</h4>
+      <p>
+        Empowered development teams to work efficiently, improve code quality,
+        and accelerate product delivery through technical expertise and
+        leadership. Spearheaded development and maintenance of a unified design
+        system, driving consistency, scalability, and accessibility across UI
+        components, and fostering collaboration between design and engineering
+        teams.
+      </p>
+      <p>
+        Technical Achievements - Modernized legacy code, upgrading React to v18,
+        and optimizing CI processes with GitHub Actions, resulting in a
+        reduction in feature delays and faster developer workflows Leadership &
+        - Developed responsive, WCAG-compliant components using React and CSS,
+        enhancing user experience. - Streamlined workflows with Storybook and
+        Figma integration, ensuring consistency. Ensured code integrity with Git
+        version control.
+      </p>
+      <p>
+        Collaboration - Led on-call shifts, resolving critical issues and
+        ensuring clear communication. - Developed and delivered on-call
+        training, enhancing team readiness. - Collaborated with design team to
+        revamp website navigation, addressing accessibility.
+      </p>
+      <p>
+        Communication & Innovation - Presented best practices to engineering
+        organization, promoting awareness and alignment. - Embedded with product
+        teams, accelerating launches.
+      </p>
 
-            <h3 className={'Work-history-title'}>
-                Oracle (acquired AddThis) - Principal Front End Engineer
-            </h3>
-            <h4 className={'Work-history-sub-title'}>
-                March 2015 - April 2018 / 3 years
-            </h4>
-            <p>Work closely with designers to implement their mocks for an application where users configure banners and alerts they want to show up on different sites.</p>
-            <p>Used Project and Product Manager skills such as feature creation, handling estimations, task creation and communication of status to other teams.</p>
-            <p>Mentored Junior developers</p>
-            <p>Code reviews, testing and overall application architecture.</p>
-            <h3 className={'Work-history-title'}>
-                Yahoo - Front End Engineer
-            </h3>
-            <h4 className={'Work-history-sub-title'}>
-                May 2007 - March 2015 / 8 Years
-            </h4>
-            <p>Worked on a team that built the number one Fantasy Sports website on the Internet.</p>
-            <p>Created amazing user focused products with PHP, HTML, JavaScript and CSS, web standards and the latest web development techniques to create cross browser/device supported web sites that are also accessible to disabled users. </p>
-            <p>Wrote code that was optimized, efficient and scalable enough to support thousands of requests a second.</p>
-            <p>Onboard new hires, mentor interns, lead user studies and work with product to come up with and implement features.</p>
-
-            <h3 className={'Work-history-title'}>
-                RealtyTrac - Lead Web Developer
-            </h3>
-            <h4 className={'Work-history-sub-title'}>
-                February 2006 - May 2007 / 1 year 4 months
-            </h4>
-            <p>Lead developer and manager of a group responsible for the front-end of Realtytrac.com. I developed code using ASP, HTML, CSS and JavaScript. I spearheaded RealtyTrac's movement to web standards and accessibility. I reported to the CTO and was responsible for making sure that all front-end tasks related to the RealtyTrac.com website, intranet and email campaigns were scheduled and completed on time by my team or me.</p>
-
-            <h3 className={'Work-history-title'}>
-                Autobytel - QA Engineer > Lead Web Development Engineer > Manager, Consumer Web
-            </h3>
-            <h4 className={'Work-history-sub-title'}>
-                November 1997 - September 2005 / 8 Years
-            </h4>
-            <p>Started out with no web experience as a QA Engineer responsible for ensuring the quality of the Autobytel.com website. While working as QA Engineer I took the initiative to learn Coldfusion and HTML on my own time enabling me to be promoted to developer. From there I went on to be lead developer and finally Manager/Developer in charge of a team responsible for the code for the website.</p>
-        </section>
-    </div>
+      <h3 className={"Work-history-title"}>Medallia - Sr. Design Engineer</h3>
+      <h4 className={"Work-history-sub-title"}>April 2018 - May 2022</h4>
+      <p>
+        Working on the Alchemy design system used across Medallia and its
+        acquired companies. The design system is built in React and Typescript
+        using Styled-components, tested with React Testing Library and
+        documented on a Gatsby website.
+      </p>
+      <p>
+        Part of a small team that created the component library from scratch.
+        Because of our work Medallia has been able to unite design across their
+        websites by utilizing our pre-made UI React components and elements.
+      </p>
+      <p>
+        Creating a new component allowed me to grow several skill sets beyond my
+        core skills including API design, accessibility with a minimum WCAG 2.0
+        AA compliance and comprehensive testing using React Testing Library.
+      </p>
+      <p>
+        Extensively documented all components I worked on including examples.
+      </p>
+      <p>
+        Working closely with design on implementing their mocks to insure that
+        they stay true to the design but also take into account possible
+        browser, accessibility and ux issues that may arise.
+      </p>
+      <p>
+        Helped teams onboard to Alchemy by answering questions, code reviews,
+        debugging issues and adding requested features.
+      </p>
+      <h3 className={"Work-history-title"}>
+        Oracle (acquired AddThis) - Principal Front End Engineer
+      </h3>
+      <h4 className={"Work-history-sub-title"}>
+        March 2015 - April 2018 / 3 years
+      </h4>
+      <p>
+        Work closely with designers to implement their mocks for an application
+        where users configure banners and alerts they want to show up on
+        different sites.
+      </p>
+      <p>
+        Used Project and Product Manager skills such as feature creation,
+        handling estimations, task creation and communication of status to other
+        teams.
+      </p>
+      <p>Mentored Junior developers</p>
+      <p>Code reviews, testing and overall application architecture.</p>
+      <h3 className={"Work-history-title"}>Yahoo - Front End Engineer</h3>
+      <h4 className={"Work-history-sub-title"}>
+        May 2007 - March 2015 / 8 Years
+      </h4>
+      <p>
+        Worked on a team that built the number one Fantasy Sports website on the
+        Internet.
+      </p>
+      <p>
+        Created amazing user focused products with PHP, HTML, JavaScript and
+        CSS, web standards and the latest web development techniques to create
+        cross browser/device supported web sites that are also accessible to
+        disabled users.{" "}
+      </p>
+      <p>
+        Wrote code that was optimized, efficient and scalable enough to support
+        thousands of requests a second.
+      </p>
+      <p>
+        Onboard new hires, mentor interns, lead user studies and work with
+        product to come up with and implement features.
+      </p>
+      <h3 className={"Work-history-title"}>RealtyTrac - Lead Web Developer</h3>
+      <h4 className={"Work-history-sub-title"}>
+        February 2006 - May 2007 / 1 year 4 months
+      </h4>
+      <p>
+        Lead developer and manager of a group responsible for the front-end of
+        Realtytrac.com. I developed code using ASP, HTML, CSS and JavaScript. I
+        spearheaded RealtyTrac's movement to web standards and accessibility. I
+        reported to the CTO and was responsible for making sure that all
+        front-end tasks related to the RealtyTrac.com website, intranet and
+        email campaigns were scheduled and completed on time by my team or me.
+      </p>
+      <h3 className={"Work-history-title"}>
+        Autobytel - QA Engineer > Lead Web Development Engineer > Manager,
+        Consumer Web
+      </h3>
+      <h4 className={"Work-history-sub-title"}>
+        November 1997 - September 2005 / 8 Years
+      </h4>
+      <p>
+        Started out with no web experience as a QA Engineer responsible for
+        ensuring the quality of the Autobytel.com website. While working as QA
+        Engineer I took the initiative to learn Coldfusion and HTML on my own
+        time enabling me to be promoted to developer. From there I went on to be
+        lead developer and finally Manager/Developer in charge of a team
+        responsible for the code for the website.
+      </p>
+    </section>
+  </div>
 );
 
 export default Work;
